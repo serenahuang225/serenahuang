27ca2a18fbd56fdba16ac345322a46a7aaa43090
serenahuang225 <serenahuang225@gmail.com>
2024-11-01 17:24:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useLocation, useRoutes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { cloneElement } from "react";
+import Home from "./routes/Home";
 
 function App() {
+  const element = useRoutes([
+    {
+      path:"/",
+      element: <Home />
+    },
+    {
+      path:"/home",
+      element: <Home />
+    }
+  ])
+
+  const location = useLocation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AnimatePresence mode='wait'>
+      {cloneElement(element, { key: location.pathname })}
+    </AnimatePresence>
   );
 }
 
